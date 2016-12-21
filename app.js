a390ba0c64d6014c0f57cd700458081aa2335de1
jeffdiers <jeff.diers@gmail.com>
2016-12-21 11:41:50 -0700

    Add api

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const app = express()
 
 //app.METHOD(path, handler)
 app.get('/', function(req, res){
-  res.send("Hello world")
+  res.send("Hello bitches")
+})
+
+app.get('/api', (req, res) => {
+  let products = [
+    {name: "Javascript Book", price: 9.99},
+    {name: "Christman Carol", price: 19.99},
+  ]
+  let stringifyed = JSON.stringify(products)
+  res.send(stringifyed)
 })
 
 app.listen(3000, function(){
