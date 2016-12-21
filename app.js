f89a3a3b7006c9a421b3f31150f78f9d3c9aaba3
jeffdiers <jeff.diers@gmail.com>
2016-12-21 15:29:15 -0700

    post and add to array

--- app.js
+++ app.js
@@ -3,15 +3,23 @@ const app = express()
 
 //app.METHOD(path, handler)
 app.get('/', function(req, res){
-  res.send("Hello bitches")
+  res.send("Hello hey")
 })
 
 app.get('/api', (req, res) => {
-  let products = [
-    {name: "Javascript Book", price: 9.99},
-    {name: "Christman Carol", price: 19.99},
-  ]
   let stringifyed = JSON.stringify(products)
+  res.send(stringifyed)
+})
+
+let products = [
+  {id: 1, name: "Javascript Book", price: 9.99},
+  {id: 2, name: "Christman Carol", price: 19.99},
+]
+
+app.get('/api/products/:id', (req, res) => {
+  let id = Number(req.params.id)
+  let product = products.find((element) => element.id === id)
+  let stringifyed = JSON.stringify(product)
   res.send(stringifyed)
 })
 
